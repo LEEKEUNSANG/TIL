1f64ab86e1f84834bf21ed5aea65b10f206bac4b
LEEKEUNSANG <leekeunsang1994@gmail.com>
2021-06-02 18:58:28 +0900

    오타 수정 vuepress -> /vuepress/

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,7 +2,7 @@
 module.exports = {
     title: '숨쉬고 눈깜빡이고 침삼켜', // 사이트 타이틀
     description: 'vuepress로 만든 문서입니다.',
-    base: 'vuepress',
+    base: '/vuepress/',
     dest: 'build',
     themeConfig: {
       logo: 'https://user-images.githubusercontent.com/82923245/120453262-d984c080-c3cd-11eb-8290-1192553c9684.jpg', // 로고 이미지
