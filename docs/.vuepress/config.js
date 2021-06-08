3e46a5c242f6f84aecd5c1af3b86dd1b1cae461c
LEEKEUNSANG <leekeunsang1994@gmail.com>
2021-06-08 14:01:23 +0900

    ajxa 예문

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,7 +17,8 @@ module.exports = {
           '/Study_Ajax/DOM/DOM',
           '/Study_Ajax/XMLHttpRequest/XMLHttpRequest',
           '/Study_Ajax/request/request',
-          '/Study_Ajax/response/response'
+          '/Study_Ajax/response/response',
+          '/Study_Ajax/ajax_example/'
         ]},
       ],
   }
